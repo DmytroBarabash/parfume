96abff301bfdd8b1fc73a562314b0f128a708007
Dmytro Barabash <d.barabash@gmail.com>
2014-09-30 22:43:57 +0300

    system.out -> logger

--- src/main/java/ua/com/parfumkatalog/ExcelImporter.java
+++ src/main/java/ua/com/parfumkatalog/ExcelImporter.java
@@ -1,42 +1,46 @@
 package ua.com.parfumkatalog;
 
-import java.io.FileInputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import org.apache.log4j.Logger;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * @author <a href="mailto:dmytro.barabash@playtech.com"> Dmytro Barabash</a> 2013-09-06 15:45
  */
 public class ExcelImporter {
 
-	public List<List<HSSFCell>> importExcelSheet(String fileName) {
-		List<List<HSSFCell>> result = new ArrayList<List<HSSFCell>>();
-		try {
-			Workbook workBook = WorkbookFactory.create(new FileInputStream(fileName));
-			Sheet sheet = workBook.getSheetAt(0);
-			Iterator rowIter = sheet.rowIterator();
+    private final Logger logger = Logger.getLogger(getClass());
 
-			while (rowIter.hasNext()) {
-				HSSFRow row = (HSSFRow)rowIter.next();
-				Iterator cellIter = row.cellIterator();
-				List<HSSFCell> cellStoreVector = new ArrayList<HSSFCell>();
+    public List<List<HSSFCell>> importExcelSheet(String fileName) {
+        List<List<HSSFCell>> result = new ArrayList<List<HSSFCell>>();
+        try {
+            Workbook workBook = WorkbookFactory.create(new FileInputStream(fileName));
+            Sheet sheet = workBook.getSheetAt(0);
+            Iterator rowIterator = sheet.rowIterator();
 
-				while (cellIter.hasNext()) {
-					HSSFCell cell = (HSSFCell)cellIter.next();
-					cellStoreVector.add(cell);
-				}
-				result.add(cellStoreVector);
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		return result;
-	}
+            while (rowIterator.hasNext()) {
+                HSSFRow row = (HSSFRow) rowIterator.next();
+                Iterator cellIterator = row.cellIterator();
+                List<HSSFCell> cellStoreVector = new ArrayList<HSSFCell>();
+
+                while (cellIterator.hasNext()) {
+                    HSSFCell cell = (HSSFCell) cellIterator.next();
+                    cellStoreVector.add(cell);
+                }
+                result.add(cellStoreVector);
+            }
+        } catch (Exception ex) {
+            logger.error("Couldn't parse file '" + fileName + "'", ex);
+        }
+        return result;
+    }
 
 }
